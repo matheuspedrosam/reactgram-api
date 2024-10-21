381b496371a64db6419a6ce494f96243e765591f
matheuspedrosam <matheuspedrosa2002@gmail.com>
2024-10-21 17:44:23 -0300

    .env-example

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const dbUrl = process.env.DB_URL;
-// "mongodb://localhost:27017/reactgram" (OLD)
 async function main() {
     await mongoose.connect(dbUrl);
     console.log("Conectou ao mongoDB com Mongoose!");
